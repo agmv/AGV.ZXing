42c1130ca3482dd0bab0815cb0c68a3f5422adb8
André Vieira <andre.g.vieira@gmail.com>
2023-04-14 21:23:31 +0100

    Fixed publish to target linux-x64 Fixed dependencies

--- generate_upload_package.ps1
+++ generate_upload_package.ps1
@@ -1,2 +1,2 @@
-dotnet publish -c Release
-Compress-Archive -Path .\AGV.ZXing\bin\Release\net6.0\publish\* -DestinationPath ZXingLib.zip -Force
+dotnet publish AGV.ZXing -c Release -r linux-x64 --self-contained false
+Compress-Archive -Path .\AGV.ZXing\bin\Release\net6.0\linux-x64\publish\* -DestinationPath ZXingLib.zip -Force
