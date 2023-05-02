d28e328f154a6129846b229d5680892a6d1b73cc
André Vieira <andre.g.vieira@gmail.com>
2023-05-02 15:38:49 +0100

    remove temporary fix for DLL

--- generate_upload_package.ps1
+++ generate_upload_package.ps1
@@ -1,3 +1,2 @@
 dotnet publish AGV.ZXing -c Release -r linux-x64 --self-contained false
-#Remove-Item -Path .\AGV.ZXing\bin\Release\net6.0\linux-x64\publish\OutSystems.ExternalLibraries.SDK.dll
 Compress-Archive -Path .\AGV.ZXing\bin\Release\net6.0\linux-x64\publish\* -DestinationPath ZXingLib.zip -Force
