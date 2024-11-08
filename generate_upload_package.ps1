1aa1d06218f26e16cbbcd101bebbfda536db2bc9
André Vieira <andre.g.vieira@gmail.com>
2024-11-08 19:24:22 +0000

    .NET 8.0 update

--- generate_upload_package.ps1
+++ generate_upload_package.ps1
@@ -1,2 +1,2 @@
 dotnet publish AGV.ZXing -c Release -r linux-x64 --self-contained false
-Compress-Archive -Path .\AGV.ZXing\bin\Release\net6.0\linux-x64\publish\* -DestinationPath ZXingLib.zip -Force
+Compress-Archive -Path .\AGV.ZXing\bin\Release\net8.0\linux-x64\publish\* -DestinationPath ZXingLib.zip -Force
