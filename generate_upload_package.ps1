8a7da35669d21d481d85a70b24852a362a951e9a
André Vieira <andre.g.vieira@gmail.com>
2025-11-11 13:26:47 +0000

    Update package script to clean up publish before a new publish

--- generate_upload_package.ps1
+++ generate_upload_package.ps1
@@ -1,2 +1,4 @@
+Remove-Item -Path ZXingLib.zip -ErrorAction SilentlyContinue
+Remove-Item -Path .\AGV.ZXing\bin\Release\net8.0\linux-x64\publish -Recurse -ErrorAction SilentlyContinue
 dotnet publish AGV.ZXing -c Release -r linux-x64 --no-self-contained
 Compress-Archive -Path .\AGV.ZXing\bin\Release\net8.0\linux-x64\publish\* -DestinationPath ZXingLib.zip -Force
